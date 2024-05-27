10d91e0cb9727c294a7fae7f11c0c934c71b5836
Hamza Khalid <hamza.khalid96804@gmail.com>
2024-05-27 18:40:57 +0500

    Update: "Addd scss"

---
+++ src/Components/Common/ListGridView/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./style.scss";
+import ListIconSVG from "../../../Icons Svg Components/ListIconSVG";
+import GridIconSVG from "../../../Icons Svg Components/GridIconSVG";
+
+function ListGridView() {
+  const [isActive, setIsActive] = useState(null);
+
+  const handleClick = (buttonIndex) => () => {
+    setIsActive(buttonIndex);
+    console.log("Active:", buttonIndex);
+  };
+
+  return (
+    <div className="button-container">
+      <button className={"button"} onClick={handleClick(0)}>
+        <div className="icon-container">
+          <ListIconSVG />
+        </div>
+        <h1 className="button-text">List</h1>
+      </button>
+
+      <button className={"button"} onClick={handleClick(1)}>
+        <div className="icon-container">
+          <GridIconSVG />
+        </div>
+        <h1 className="button-text">Grid</h1>
+      </button>
+    </div>
+  );
+}
+
+export default ListGridView;
--- src/Pages/Dashboard/Index.jsx
+++ src/Pages/Dashboard/Index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "../../Components/Common/Header/index";
-import ListGridView from "../../Separate Components/ListGridView";
 import NewSurvey from "../../Components/NewSurvey";
 import StartSurveying from "../../Components/StartSurveying";
 import { SearchTemplates } from "../../Components/Common/SearchTemplate";
 import SurveyTemplates from "../../Components/Common/SurveyTemplates";
 import "./style.scss"; // Import SCSS file for Dashboard styles
+import ListGridView from "../../Components/Common/ListGridView";
 
 function Dashboard() {
   return (
