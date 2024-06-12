781bd6f664250b8e72b7b1fcd8df27ec36af89ae
azannoor <azaanoor18@.com>
2024-06-12 01:34:51 -0700

    update:"updated left and right menu in survey dashboard."

--- src/Components/SurveyDashboard/LeftMenu/index.jsx
+++ src/Components/SurveyDashboard/LeftMenu/index.jsx
@@ -4,12 +4,7 @@ import './style.scss'; // Updated import
 function LeftMenu() {
   return (
     <div className="left-menu">
-      <div className="left-menu child1">
-        <p>Survey Presenting</p>
-      </div>
-      <div className="left-menu child2">
-        <p>You don't have any data yet.</p>
-      </div>
+      
     </div>
   );
 }
