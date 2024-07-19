a7d3ce4919d6def4f66a0329ab795b83a0f8d62e
Muhammad Hammad <hammadkhalil04@.com>
2024-07-19 12:23:02 +0500

    Feat: "Added GPT in Ai builder Page"

--- src/Pages/BuildServeyWithAi/index.jsx
+++ src/Pages/BuildServeyWithAi/index.jsx
@@ -1,4 +1,3 @@
-
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,12 +6,88 @@ import React from "react";
 import Header from "../../Components/Common/Header";
 import "./style.scss";
 import SvgRenderer from "../../Svg Components";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
 import { useState } from "react";
 
 function BuildSurveyWithAi() {
-  const [showPrompts, setShowPrompts] = useState(false);
- 
- 
+  const API_Key = ""; //Make API Key
+
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello,I am ChatGPT",
+      sender: "ChatGPT",
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing",
+    };
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+
+    setTyping(true);
+
+    async function processMessageToChatGPT(chatMessages) {
+      let apiMessage = chatMessages.map((messageObject) => {
+        let role = "";
+        if (messageObject.sender === "ChatGPT") {
+          role = "assisttan";
+        } else {
+          role = "user";
+        }
+        return { role: role, content: messageObject.message };
+      });
+
+      const systemMessage = {
+        role: "system",
+        content: "I want you to create Survey", //How u want GPT to behave
+      };
+
+      const apiRequestBody = {
+        model: "gpt-3.5-turbo",
+        messages: [systemMessage, ...apiMessages],
+      };
+
+      await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer" + API_Key,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(apiRequestBody),
+      })
+        .then((data) => {
+          return data.json();
+        })
+        .then((data) => {
+          console.log(data);
+          console.log(data.choices[0].message.content);
+          setMessages([
+            ...chatMessages,
+            {
+              message: data.choices[0].message.content,
+              sender: "ChatGPT",
+            },
+          ]);
+          setTyping(false);
+        });
+    }
+  };
+
   return (
     <>
       <div>
@@ -29,28 +104,44 @@ function BuildSurveyWithAi() {
             </div>
           </div>
         </div>
+
         <div className="prompt-input-container">
           <p className="prompt-input-title">
             Tell us what you want to build Survey Form with AI Prompt:
           </p>
-          <form className="prompt-input-box  border-2 rounded-lg">
-            <textarea
-              type="text"
-              className="form-textarea"
-              placeholder="Enter your prompt here"
-            />
-            <button type="submit" className="form-submit-button">
-              Submit
-            </button>
-          </form>
+
+          <div className="gpt-box">
+            {" "}
+            <MainContainer>
+              <ChatContainer>
+                <MessageList
+                  typingIndicator={
+                    typing ? (
+                      <TypingIndicator content="ChatGPT is typing" />
+                    ) : null
+                  }
+                >
+                  {messages.map((message, i) => {
+                    return <Message key={i} model={message} />;
+                  })}
+                </MessageList>
+                <MessageInput
+                  placeholder="Type Message Here"
+                  onSend={handleSend}
+                />
+              </ChatContainer>
+            </MainContainer>
+          </div>
         </div>
+
         <div className="dropdown-container">
-          <p>Tips</p>
+          <p className="tip-heading">Tips</p>
           <div className="tips">
             <p>
               Clearly state your question with all necessary details to avoid
               ambiguity.
             </p>
+
             <p>
               Include examples or show what you’ve tried to help clarify your
               request.
@@ -73,6 +164,7 @@ function BuildSurveyWithAi() {
             </p>
           </div>
         </div>
+
         <div className="row promp-templates">
           <div className=" promps-container">
             <h1>Example 1</h1>
@@ -102,4 +194,5 @@ function BuildSurveyWithAi() {
     </>
   );
 }
+
 export default BuildSurveyWithAi;
